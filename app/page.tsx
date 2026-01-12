ff4f8fb78bbc771ceb8971162ae4522724883a79
GarciaGst126-arch <lopezhigo126@gmail.com>
2026-01-11 18:14:10 -0600

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { SpamDetector } from "@/components/spam-detector"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <SpamDetector />
+    </main>
+  )
+}
